b0a52244012a1ea6630d934874cbc1696a2fc4e7
Halfapear <1783148122@qq.com>
2023-10-27 22:45:02 +0800

    我又出了个错误：其实模块化又是有问题的。。又来新问题了。。其次 这个版本 不会显示password ，估计和那几个输入没有任何限制于是快速略过了。 接下来先把模块化的问题修复一下然后再测试一下前面的代码

--- main.c
+++ main.c
@@ -5,30 +5,37 @@
 
 
 void main(){
-		int Password = 0,KeyNum,n[4],i;
+        int Password = 0,KeyNum,n[4],i;
 		LCD_Init();
 		LCD_ShowString(1,1,"Password");
         while(1){
             //??while ????,????????,??????Password???Password???????f u
             // if(KeyNum)
             //     {
-                for(i=0;i<4;i++){
-                        n[i]=MatrixKey();
-                        Password =Password *10 + n[i];
-                        LCD_ShowNum(2,1,Password,4);
-                        }
-                if(KeyNum==11){
+                // for(i=0;i<4;i++){
+                //         n[i]=MatrixKey();
+                //         Password =Password *10 + n[i];
+                //         LCD_ShowNum(2,1,Password,4);
+                //         }
+                n[0]=MatrixKey();
+                LCD_ShowNum(2,1,Password,4);
+                n[1]=MatrixKey();
+                LCD_ShowNum(2,1,Password,4);
+                n[2]=MatrixKey();
+                LCD_ShowNum(2,1,Password,4);
+                n[3]=MatrixKey();
+                LCD_ShowNum(2,1,Password,4);
+                Password =n[3]*1000 + n[2]*100 + n[1]*10 + n[0]*1;
+                // if(KeyNum==11){
                         if (Password == 628){
-														LCD_Init;
                                 LCD_ShowString(1,1,"yeah");
                             while(1){}
                         }
                         else{
-														LCD_Init;
-                            LCD_ShowString(1,1,"f**k you");
+                                LCD_ShowString(1,1,"f**k you");
                             while(1){}
                         }
-                }
+                // }
 		    
 		}
 }
