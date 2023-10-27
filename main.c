1acff4197a7ab0e6f0ed77f91e95a486fb8462ce
Halfapear <1783148122@qq.com>
2023-10-27 21:30:13 +0800

    second

--- main.c
+++ main.c
@@ -5,27 +5,28 @@
 
 
 void main(){
-		int Password=0,KeyNum,n[4],i;
+        int Password = 0,KeyNum,n[4],i;
 		LCD_Init();
-		LCD_ShowString(1,1,"Password");
-		while(1){
-				if(KeyNum)
-						{
-						for(i=0;i<4;i++){
-								Password = Password *10;
-								n[i]=MatrixKey();
-								Password =Password + n[i];
-								}
-						if(Password==1234){
-								if (Password == 628){
-										LCD_ShowString(1,1,"yeah");
-									while(1){}
-								}
-								else{
-										LCD_ShowString(1,1,"f**k you");
-									while(1){}
-								}
-							}
-			}
-}
+		LCD_ShowString(1,1,"Password");\
+        while(1){
+            //??while ????,????????,??????Password???Password???????f u
+            if(KeyNum)
+                {
+                for(i=0;i<4;i++){
+                        Password = Password *10;
+                        n[i]=MatrixKey();
+                        Password =Password + n[i];
+                        }
+                if(KeyNum==11){
+                        if (Password == 628){
+                                LCD_ShowString(1,1,"yeah");
+                            while(1){}
+                        }
+                        else{
+                                LCD_ShowString(1,1,"f**k you");
+                            while(1){}
+                        }
+                }
+		    }
 		}
+}
