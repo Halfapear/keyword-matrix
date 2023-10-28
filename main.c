da2c5ecb117522b7689cfe3e6244cb8763463ff1
Halfapear <1783148122@qq.com>
2023-10-28 22:05:52 +0800

    终于。。。明明不需要这么长时间的，总结一下最后就是for删掉，各种没有赋值的（这是最重点的）所以，以后一点点做！

--- main.c
+++ main.c
@@ -5,28 +5,25 @@
 
 
 void main(){
-        int Password = 0,KeyNum,n[4],i;
+        int Password = 0,KeyNum,n,i;
 		LCD_Init();
 		LCD_ShowString(1,1,"Password");
         while(1){
+					KeyNum = MatrixKey();
             //??while ????,????????,??????Password???Password???????f u
-            // if(KeyNum)
-            //     {
-                // for(i=0;i<4;i++){
-                //         n[i]=MatrixKey();
-                //         Password =Password *10 + n[i];
-                //         LCD_ShowNum(2,1,Password,4);
-                //         }
-                n[0]=MatrixKey();
-                LCD_ShowNum(2,1,Password,4);
-                n[1]=MatrixKey();
-                LCD_ShowNum(2,1,Password,4);
-                n[2]=MatrixKey();
-                LCD_ShowNum(2,1,Password,4);
-                n[3]=MatrixKey();
-                LCD_ShowNum(2,1,Password,4);
-                Password =n[3]*1000 + n[2]*100 + n[1]*10 + n[0]*1;
-                // if(KeyNum==11){
+            if(KeyNum)
+                {
+//                for(i=0;i<4;i++){
+//									KeyNum = MatrixKey();
+//									if(KeyNum){
+									if(KeyNum <=10){
+                        Password = Password *10;
+                        Password =Password + KeyNum%10;
+									}
+//                        }
+//											}
+									
+                if(KeyNum==11){
                         if (Password == 628){
                                 LCD_ShowString(1,1,"yeah");
                             while(1){}
@@ -35,7 +32,7 @@ void main(){
                                 LCD_ShowString(1,1,"f**k you");
                             while(1){}
                         }
-                // }
-		    
+                }
+		    }
 		}
 }
